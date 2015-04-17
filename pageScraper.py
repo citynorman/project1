09461420fa954b964891786a23350569be98e025
rgit1 <rgit1@users.noreply.github.com>
2015-04-17 00:16:05 -0400

    Update pageScraper.py

--- pageScraper.py
+++ pageScraper.py
@@ -2,12 +2,14 @@ from bs4 import BeautifulSoup
 from bs4 import Tag
 
 import urllib2
+import MySQLdb
 
 from dataSheetItem import DataSheetItem
 
 class PageScraper ():
 
-    def scrape(self, url, holdingDate): #Change this name to reportDate. The date needs to be provided.
+
+    def scrape(self, url, holdingDate): #Change this name to reportDate.
 
         holdingCompanyCIK = url.split("/")[6] #This is the CIK number via which the company is identified in the EDGAR database.
 
@@ -16,6 +18,12 @@ class PageScraper ():
         soup = BeautifulSoup (page.read(), "xml")
 
         entries = soup.findAll('infoTable')
+
+        #dsi = DataSheetItem() #This should be done VERY differently, though Python init's are different from Java/etc. constructors.
+
+        # Open database connection
+        db = MySQLdb.connect("localhost","testuser","password","mytestdb")
+
 
         for entry in entries:
             #try:
@@ -27,8 +35,8 @@ class PageScraper ():
             shPrn = entry.find('sshPrnamtType')
             putCall = entry.find('putCall') #This may often be a None.
             investmentDiscretion = entry.find('investmentDiscretion')
-            otherManager = entry.find('otherManager') #Most often a None.
-            soleVotingAuthority = entry.find('Sole') 
+            otherManager = entry.find('otherManager')
+            soleVotingAuthority = entry.find('Sole') #This is a bit of a hack.
             sharedVotingAuthority = entry.find('Shared')
             noneVotingAuthority = entry.find('None')
             #except:
@@ -50,9 +58,12 @@ class PageScraper ():
                 sharedVotingAuthority,
                 noneVotingAuthority
                 )
-            try:
-                dsi.saveToDB() #Not the most efficient way to deal with database connections.
+            try:                
+                dsi.saveToDB(db) #Not the most efficient way to deal with database connections!
             except Exception, e:
                 print str(e)
                 print("Failed to save to DB.")
-                
+
+                # disconnect from server
+        db.close()
+                   
